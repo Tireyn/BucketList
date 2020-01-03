b495f07fe3db03fc9ea09c52c44ba0b1d636c5ba
Sam McCullough <samanuel.mc@gmail.com>
2020-01-03 00:27:39 -0800

    Challenge 2

---
+++ BucketList/ContentDetailView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentDetailView.swift
+//  BucketList
+//
+//  Created by Sam on 2020-01-02.
+//  Copyright © 2020 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentDetailView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [CodableMKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    @State private var showingEditScreen = false
+    
+    var body: some View {
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.title = "Add a title."
+                        newLocation.subtitle = "Add a description."
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                        
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                    self.showingEditScreen = true
+                })
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save")
+        }
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentDetailView()
+    }
+}
