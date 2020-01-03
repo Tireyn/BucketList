2ce3d46ecf4e789888dc9d4295afe8dbf34d5d6d
Sam McCullough <samanuel.mc@gmail.com>
2020-01-03 00:37:49 -0800

    Removed print out of good Wikipedia URL.

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -52,7 +52,6 @@ struct EditView: View {
     
     func fetchNearbyPlaces() {
         let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        print("URL: \(urlString)")
         
         guard let url = URL(string: urlString) else {
             print("Bad URL: \(urlString)")
